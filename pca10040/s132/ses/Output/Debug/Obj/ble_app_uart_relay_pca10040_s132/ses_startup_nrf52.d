94e953dc22dede910e6ab0eeced7459cc86669e7
Guillermo Rojas <apemangr@gmail.com>
2025-09-23 12:55:37 -0300

    Firmware funcionando pero con ausencia de algunas funcionalidades

---
+++ pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_relay_pca10040_s132/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_uart_relay_pca10040_s132/ses_startup_nrf52.o: \
+ C\:\Users\guill\OneDrive\Desktop\SDK_CON_FIRMWARE\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf52.s
---
+++ pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_relay_pca10040_s132/utf.d
@@ -0,0 +1,6 @@
+Output/Debug/Obj/ble_app_uart_relay_pca10040_s132/utf.o: \
+ C\:\Users\guill\OneDrive\Desktop\SDK_CON_FIRMWARE\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.c \
+ C\:\Users\guill\OneDrive\Desktop\SDK_CON_FIRMWARE\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stddef.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/__crossworks.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h
